89a0feb98d2b3191252d2ba3c1113ac47b139cd9
IvanKun <ivankuninets@Ivans-MacBook-Air.local>
2024-12-17 12:16:18 -0300

    Third Lesson Done

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+
+var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+var ypGrey: UIColor { UIColor(named: "YP Grey") ?? UIColor.gray }
+var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+    
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,5 +1,4 @@
 import UIKit
-
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
